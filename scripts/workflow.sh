b535996ae6e4e6f5ba4c534b834d4bf986ba3fec
croushore <117045434+croushore@users.noreply.github.com>
2024-05-09 11:28:06 -0500

    Update workflow.sh

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -1,22 +1,28 @@
+## make a new “master folder” called anything, i’m calling this one “proj” for convenience
+mkdir proj
+cd proj
+
 ## setup
 workdir=$(pwd)
 cd $workdir
-module load stack/2021.1-base_arch   # See https://uiowa.atlassian.net/wiki/spaces/hpcdocs/pages/76515023/2021.1+Stack for the list of modules loaded
+module load stack/2021.1-base_arch
 
 ## download fastq.gz files
 mkdir fastq  # this makes a new folder called "fastq" where we will download our files
-wget -P $workdir/fastq -r -l 2 -nH -nd -np --ignore-case -A '*fastq.gz' [insert link]  # this command will recursively download all files ending with ".fastq.gz" from the specified URL (http://example.edu/results/), up to two levels deep, ignoring case and saving them in the current directory without creating a directory structure mirroring the server.
+
+wget -P $workdir/fastq -r -l 2 -nH -nd -np --ignore-case -A '*fastq.gz' http://dnacore454.healthcare.uiowa.edu/20230121-0382_Gordon_23002aMxHLSHcKuowawEVjRVzQVXpPXHnImADkEVibTZw/results/Project_Gordon_23002/
 
 ## quality checking fastq.gz files
-mkdir fastq/qc # this makes a new folder called "qc" inside the "fastq" folder to output fastqc files into
-fastqc -o $workdir/fastq/qc -t 6 *fastq.gz  # this command will execute FastQC on all FASTQ files ending with ".fastq.gz" in the current directory, using up to 6 CPU threads for analysis, and save the resulting reports to the directory specified
-multiqc $workdir/fastq/qc  # multiqc compiles all individual fastqc files into one report
+mkdir fastq/qc
+mkdir fastq/multiqc
+fastqc fastq/*.fastq.gz -o fastq/qc 
+multiqc fastq/qc -o fastq/multiqc # multiqc compiles all individual fastqc files into one report
 
 ## STAR alignment
 mkdir star
 cd fastq
 
-staridx=$workdir/hs_index_100/hs_genome
+staridx=/Users/croushore/hs_index
 
 module load stack/2022.2-base_arch
 
@@ -25,20 +31,46 @@ for i in *_R1_001.fastq.gz; do
   --genomeLoad NoSharedMemory \
   --readFilesCommand zcat \
   --outSAMtype BAM SortedByCoordinate \
-  --genomeDir $staridx \
+  --genomeDir $staridx/hs_genome \
   --readFilesIn $i ${i%_R1_001.fastq.gz}_R2_001.fastq.gz \
   --runThreadN 6  \
   --outSAMattributes NM  \
-  --outFileNamePrefix $workdir/STAR/${i%_R1_001.fastq.gz}; done
+  --outFileNamePrefix $workdir/star/${i%_R1_001.fastq.gz}; done
 
 ## generate read counts
-cd $workdir
-mkdir counts
-#module load stack/2022.2-base_arch
+cd $workdir/star
+mkdir $workdir/counts
 
-featureCounts -T 4 -s 2 -p -t exon -g gene_id -a $workdir/hs_index_100/gencode.v29.annotation.gtf -o $workdir/counts/counts.txt *.bam
+featureCounts -T 4 -s 2 -p -t exon -g gene_id -a $staridx/gencode.v45.annotation.gtf -o $workdir/counts/counts.txt *.bam
 
 multiqc $workdir/counts/counts.txt.summary
 
-## tidy data in R
-Rscript $workdir/scripts/tidy_counts.R
+## open R and tidy data
+
+module load stack/2022.1-base_arch
+module load r/4.1.3_gcc-9.4.0
+cd $workdir/counts
+
+R
+
+install.packages(c("tidyverse", "magrittr","stringr","dplyr"),repos = "http://cran.us.r-project.org")
+
+library(tidyverse)
+library(magrittr)
+library(stringr)
+library(dplyr)
+
+## load counts.txt file for data tidying
+counts <- read.delim("counts.txt", comment.char="#", header= TRUE, row.names = 1)
+
+## removing ENSEMBL version numbers (e.g., ENG0898O.1 -> ENGENG0898O)
+row.names(counts) %<>% str_remove("\\.[0-9]+$")
+
+## removing Chr, Start, End, Strand, and Length columns (for easy iDEP analysis)
+counts_tidy <- counts[,c(-(1:5))]
+
+## tidying sample column names
+names(counts_tidy) = gsub(pattern = "_.*", replacement = "", x = names(counts_tidy))
+
+## writing as csv file (raw counts)
+write.csv(counts_tidy, file = "counts_tidy.csv")
