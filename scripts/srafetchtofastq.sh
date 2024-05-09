bdf649553376ded5f224ba48b197a7a4aeb9b7b4
croushore <117045434+croushore@users.noreply.github.com>
2024-05-09 11:34:05 -0500

    Update srafetchtofastq.sh

--- scripts/srafetchtofastq.sh
+++ scripts/srafetchtofastq.sh
@@ -7,13 +7,16 @@
 #            Regulatory Axis Confers Ewing Cells Exquisite Sensitivity to Statins. 
 #            Cancers (Basel) 2022 May 8;14(9). PMID: 35565457
 
+## make a new “master folder” called anything, i’m calling this one “GSE164372” for convenience
+mkdir GSE164372
+cd GSE164372
+
+## setup
 workdir=$(pwd)
 cd $workdir
-mkdir GSE164372
 
 module load stack/2022.2-base_arch # See https://uiowa.atlassian.net/wiki/spaces/hpcdocs/pages/76513504/2022.2+stack for the list of modules loaded
 module load sra-tools/3.0.3_gcc-9.5.0
-cd $workdir/GSE164372
 
 # select SRR files of interest
 for (( i = 43; i <= 60; i++ ))
